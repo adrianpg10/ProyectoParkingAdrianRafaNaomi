00430362def6d98b723598bd954a59e41ec54188
Adrianpg <adrianperezgomezsmr@gmail.com>
2019-05-16 18:46:46 +0200

    Creacion del interface de tickets

---
+++ src/main/java/daw/tickets/ITickets.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.tickets;
+
+import daw.reservas.ReservasVO;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author adrip
+ */
+public interface ITickets {
+  // Método para obtener todos los registros de la tabla
+    List<TicketsVO> getAll() throws SQLException;
+    
+    // Méodo para obtener un registro a partir de la PK
+    TicketsVO findByPk(int numplaza, String matricula) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertTickets (TicketsVO tickets) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertTickets (List<TicketsVO> lista) throws SQLException;
+    
+    // Método para borrar una categoria
+    int deleteTickets (TicketsVO t) throws SQLException;
+    
+    // Método para borrar toda la tabla
+    int deleteTickets() throws SQLException;
+    
+    // Método para modificar una tickets. Se modifica a la persona que tenga esa 'pk'
+    // con los nuevos datos que traiga la persona 'nuevosDatos'
+    int updateTickets (int numplaza, String matricula , ReservasVO nuevosDatos) throws SQLException;   
+}
