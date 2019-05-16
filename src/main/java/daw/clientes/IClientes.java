ddb030064f73c8ff95de1dd493364f3881b00c19
Rafa <rafaelcarrillobonilla@gmail.com>
2019-05-16 20:01:53 +0200

    Implementa la interfaz de clientes con sus respectivos metodos

---
+++ src/main/java/daw/clientes/IClientes.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.clientes;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Rafa
+ */
+public interface IClientes {
+     // Método para obtener todos los registros de la tabla
+    List<ClientesVO> getAll() throws SQLException;
+    
+    // Méodo para obtener un registro a partir de la PK
+    ClientesVO findByPk(int x) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertClientes (ClientesVO p) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertClientes (List<ClientesVO> lista) throws SQLException;
+    
+    // Método para borrar una categoria
+    int deleteClientes (ClientesVO p) throws SQLException;
+    
+    // Método para borrar toda la tabla
+    int deleteClientes() throws SQLException;
+    
+    // Método para modificar una persona. Se modifica a la persona que tenga esa 'pk'
+    // con los nuevos datos que traiga la persona 'nuevosDatos'
+    int updateClientes (int x, ClientesVO nuevosDatos) throws SQLException;
+}
