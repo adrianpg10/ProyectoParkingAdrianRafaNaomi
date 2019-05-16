90b3939f853366de8b184b295a15b3298ffc8180
Adrianpg <adrianperezgomezsmr@gmail.com>
2019-05-16 14:09:58 +0200

    Realizamos la conexion a la base de datos

--- src/main/java/daw/parking/datos/Conexion.java
+++ src/main/java/daw/parking/datos/Conexion.java
@@ -5,10 +5,44 @@
  */
 package daw.parking.datos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author rafa
  */
 public class Conexion {
+     private static final String SERVIDOR = "jdbc:mysql://192.168.56.101:3306/";
+    private static final String NOMBRE_BASE_DATOS = "parking";
+    private static final String USER = "adrianpg";
+    private static final String PASS = "1234";
+
+    private static Connection instancia = null;
     
+    // Constructor privado no accesible desde otras clases
+    private Conexion() {
+
+    }
+
+    // Método de clase para acceder a la instancia del objeto Connection
+    public static Connection getInstance() {
+        // Si el objeto Connection no está creado, se crea
+        if (instancia == null) {
+            try {
+
+                // Se crea el objeto Connection	
+                instancia = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
+
+                System.out.println("Conexión realizada con éxito.");
+
+            } catch (SQLException e) {
+
+                // Error en la conexión
+                System.out.println("Conexión fallida: " + e.getMessage());
+            }
+        }
+        return instancia;
+    }
 }
