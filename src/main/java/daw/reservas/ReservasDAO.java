23e6363703b05e3c6f387c1ea8fc7d01721a5f57
Adrianpg <adrianperezgomezsmr@gmail.com>
2019-05-16 18:34:21 +0200

    Implementacion de los metodos correctamente

--- src/main/java/daw/reservas/ReservasDAO.java
+++ src/main/java/daw/reservas/ReservasDAO.java
@@ -5,7 +5,15 @@
  */
 package daw.reservas;
 
+import daw.parking.datos.Conexion;
+import daw.plazas.PlazasVO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,39 +22,172 @@ import java.util.List;
  */
 public class ReservasDAO implements IReservas {
 
+    private Connection con = null;
+
+    public ReservasDAO() {
+        con = Conexion.getInstance();
+    }
+    
     @Override
     public List<ReservasVO> getAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ReservasVO> lista = new ArrayList<>();
+
+        // Preparamos la consulta de datos mediante un objeto Statement
+        // ya que no necesitamos parametrizar la sentencia SQL
+        try (Statement st = con.createStatement()) {
+            // Ejecutamos la sentencia y obtenemos las filas en el objeto ResultSet
+            ResultSet res = st.executeQuery("select * from reservas");
+            // Ahora construimos la lista, recorriendo el ResultSet y mapeando los datos
+            while (res.next()) {
+                ReservasVO r = new ReservasVO();
+                // Recogemos los datos de la persona, guardamos en un objeto
+                r.setMatricula(res.getString("matricula"));
+                r.setNumplaza(res.getInt("numplaza"));
+               r.setFeciniabono(res.getDate("feciniabono").toLocalDate());
+               r.setFeciniabono(res.getDate("fecfinabono").toLocalDate());
+                
+
+                //Añadimos el objeto a la lista
+                lista.add(r);
+            }
+    }
+        return lista;
     }
 
-    @Override
-    public ReservasVO findByPk(int matricula) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
 
     @Override
     public int insertReservas(ReservasVO reservas) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int numFilas = 0;
+        String sql = "insert into reservas values (?,?,?,?,?)";
+
+        if (findByPk(reservas.getMatricula(),reservas.getNumplaza()) != null) {
+            // Existe un registro con esa pk
+            // No se hace la inserción
+            return numFilas;
+        } else {
+            // Instanciamos el objeto PreparedStatement para inserción
+            // de datos. Sentencia parametrizada
+            try (PreparedStatement prest = con.prepareStatement(sql)) {
+
+                // Establecemos los parámetros de la sentencia
+                prest.setString(1, reservas.getMatricula());
+                prest.setInt(2, reservas.getNumplaza());
+                 prest.setString(3, reservas.getPin_fijo());
+                 prest.setString(4, reservas.getFeciniabono().format(DateTimeFormatter.ISO_DATE));
+                  prest.setString(5, reservas.getFecfinabono().format(DateTimeFormatter.ISO_DATE));
+
+                numFilas = prest.executeUpdate();
+            }
+            return numFilas;
+        }
     }
 
     @Override
     public int insertReservas(List<ReservasVO> lista) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int numFilas = 0;
+
+        for (ReservasVO tmp : lista) {
+            numFilas += insertReservas(tmp);
+        }
+
+        return numFilas;
     }
+    
 
     @Override
     public int deleteReservas(ReservasVO r) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         int numFilas = 0;
+
+        String sql = "delete from reservas where matricula = ?, numplaza= ?";
+
+        // Sentencia parametrizada
+        try (PreparedStatement prest = con.prepareStatement(sql)) {
+
+            // Establecemos los parámetros de la sentencia
+            prest.setString(1, r.getMatricula());
+            prest.setInt(2, r.getNumplaza());
+
+            // Ejecutamos la sentencia
+            numFilas = prest.executeUpdate();
+        }
+        return numFilas;
     }
 
     @Override
     public int deleteReservas() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       String sql = "delete from reservas";
+
+        int nfilas = 0;
+
+        // Preparamos el borrado de datos  mediante un Statement
+        // No hay parámetros en la sentencia SQL
+        try (Statement st = con.createStatement()) {
+            // Ejecución de la sentencia
+            nfilas = st.executeUpdate(sql);
+        }
+
+        // El borrado se realizó con éxito, devolvemos filas afectadas
+        return nfilas;
     }
 
     @Override
-    public int updateReservas(int matricula, ReservasVO nuevosDatos) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ReservasVO findByPk(String matricula, int numplaza) throws SQLException {
+         ResultSet res = null;
+        ReservasVO reservas = new ReservasVO();
+
+        String sql = "select * from reservas where matricula=?, numplaza=?";
+
+        try (PreparedStatement prest = con.prepareStatement(sql)) {
+            // Preparamos la sentencia parametrizada
+            prest.setString(1, matricula);
+            prest.setInt(2, numplaza);
+
+            // Ejecutamos la sentencia y obtenemos las filas en el objeto ResultSet
+            res = prest.executeQuery();
+
+            // Nos posicionamos en el primer registro del Resultset. Sólo debe haber una fila
+            // si existe esa pk
+            if (res.first()) {
+                // Recogemos los datos de la persona, guardamos en un objeto
+                reservas.setMatricula(res.getString("matricula"));
+                reservas.setNumplaza(res.getInt("numplaza"));
+                reservas.setPin_fijo(res.getString("pin_fijo"));
+                reservas.setFeciniabono(res.getDate("feciniabono").toLocalDate());
+                reservas.setFecfinabono(res.getDate("fecfinabono").toLocalDate());
+                return reservas;
+            }
+
+            return null;
+        }
+        
     }
-    
+
+    @Override
+    public int updateReservas(String matricula, int numplaza, ReservasVO nuevosDatos) throws SQLException {
+          int numFilas = 0;
+        String sql = "update reservas set pin_fijo = ?, feciniabono = ?,fecfinabono =?  where matricula=?, numplaza=?";
+
+        if (findByPk(matricula,numplaza) == null) {
+            // La persona a actualizar no existe
+            return numFilas;
+        } else {
+            // Instanciamos el objeto PreparedStatement para inserción
+            // de datos. Sentencia parametrizada
+            try (PreparedStatement prest = con.prepareStatement(sql)) {
+
+                // Establecemos los parámetros de la sentencia
+                prest.setString(1, nuevosDatos.getPin_fijo());
+                prest.setString(2, nuevosDatos.getFeciniabono().format(DateTimeFormatter.ISO_DATE));
+                prest.setString(3, nuevosDatos.getFecfinabono().format(DateTimeFormatter.ISO_DATE));
+                prest.setString(4, matricula);
+                prest.setInt(4, numplaza);
+
+                numFilas = prest.executeUpdate();
+            }
+            return numFilas;
+        }
+    }
+
+
 }
