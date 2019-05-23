3d3e027bcca5a681c1a48ae76c5049493e74dfe9
rafacb98 <rafaelcarrillobonilla@gmail.com>
2019-05-23 14:35:12 +0200

    Corrige error del menu al salir

--- src/main/java/daw/parking/datos/Menu.java
+++ src/main/java/daw/parking/datos/Menu.java
@@ -17,88 +17,88 @@ public class Menu {
     private static Scanner teclado = new Scanner(System.in);
     private static int opcion;
 
-
     public static Comandos menu() {
-        boolean seguir=true;
+        boolean seguir = true;
         do {
-            
-            try{
-            System.out.println("==========================================");
-            System.out.println("===============PARKING DAW================");
-            System.out.println("==========================================");
-            System.out.println("1- Entrar en la zona clientes.");
-            System.out.println("2- Entrar en la zona administrador.");
-            System.out.println("3- Salir.");
 
-            opcion = teclado.nextInt();
+            try {
+                System.out.println("==========================================");
+                System.out.println("===============PARKING DAW================");
+                System.out.println("==========================================");
+                System.out.println("1- Entrar en la zona clientes.");
+                System.out.println("2- Entrar en la zona administrador.");
+                System.out.println("3- Salir.");
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("Zona cliente. Elija una opción:");
-                    System.out.println("1- Depositar vehículo.");
-                    System.out.println("2- Retirar vehículo.");
-                    System.out.println("3- Depositar Abonado");
-                    System.out.println("4- Retirar Abonado");
-                    System.out.println("0- Volver al menú principal");
-                    opcion = teclado.nextInt();
+                opcion = teclado.nextInt();
 
-                    switch (opcion) {
-                        case 0:
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Zona cliente. Elija una opción:");
+                        System.out.println("1- Depositar vehículo.");
+                        System.out.println("2- Retirar vehículo.");
+                        System.out.println("3- Depositar Abonado");
+                        System.out.println("4- Retirar Abonado");
+                        System.out.println("0- Volver al menú principal");
+                        opcion = teclado.nextInt();
 
-                            return Comandos.VOLVER_MENU;
+                        switch (opcion) {
+                            case 0:
 
-                        case 1:
-                            return Comandos.DEPOSITAR_VEHICULO;
-                        case 2:
-                            return Comandos.RETIRAR_VEHICULO;
-                        case 3:
-                            return Comandos.DEPOSITAR_ABONADO;
-                        case 4:
-                            return Comandos.RETIRAR_ABONADO;
+                                return Comandos.VOLVER_MENU;
 
-                    }
-                    break;
+                            case 1:
+                                return Comandos.DEPOSITAR_VEHICULO;
+                            case 2:
+                                return Comandos.RETIRAR_VEHICULO;
+                            case 3:
+                                return Comandos.DEPOSITAR_ABONADO;
+                            case 4:
+                                return Comandos.RETIRAR_ABONADO;
 
-                case 2:
-                    System.out.println("Zona administrador. Elija una opción:");
-                    System.out.println("1- Consultar estado del parking.");
-                    System.out.println("2- Facturación entre fechas.");
-                    System.out.println("3- Facturación abonado");
-                    System.out.println("4- Dar de alta un abonado.");
-                    System.out.println("5- Modificar datos abonado.");
-                    System.out.println("6- Dar de baja un abonado");
-                    System.out.println("7- Datos caducidad abonado.");
-                    System.out.println("8- Realizar copia de seguridad.");
-                    System.out.println("9- Restaurar copia de seguridad");
-                    System.out.println("0-Volver al menú");
-                    opcion = teclado.nextInt();
+                        }
+                        break;
 
-                    switch (opcion) {
-                        case 0:
-                            return Comandos.VOLVER_MENU;
-                        case 1:
-                            return Comandos.ESTADO_PARKING;
-                        case 2:
-                            return Comandos.FACTURACION_ENTRE_FECHAS;
-                        case 3:
-                            return Comandos.FACTURACION_ABONADO;
-                        case 4:
-                            return Comandos.ABONO_ALTA;
-                        case 5:
-                            return Comandos.ABONO_MODIFICA;
-                        case 6:
-                            return Comandos.ABONO_BAJA;
-                        case 7:
-                            return Comandos.ABONO_CADUCIDAD;
-                        case 8:
-                            return Comandos.COPIA_SEGURIDAD_COPIAR;
-                        case 9:
-                            return Comandos.COPIA_SEGURIDAD_RESTAURAR;
-                    }
-                    break;
+                    case 2:
+                        System.out.println("Zona administrador. Elija una opción:");
+                        System.out.println("1- Consultar estado del parking.");
+                        System.out.println("2- Facturación entre fechas.");
+                        System.out.println("3- Facturación abonado");
+                        System.out.println("4- Dar de alta un abonado.");
+                        System.out.println("5- Modificar datos abonado.");
+                        System.out.println("6- Dar de baja un abonado");
+                        System.out.println("7- Datos caducidad abonado.");
+                        System.out.println("8- Realizar copia de seguridad.");
+                        System.out.println("9- Restaurar copia de seguridad");
+                        System.out.println("0-Volver al menú");
+                        opcion = teclado.nextInt();
 
-            }
-             } catch (InputMismatchException ime) {
+                        switch (opcion) {
+                            case 0:
+                                return Comandos.VOLVER_MENU;
+                            case 1:
+                                return Comandos.ESTADO_PARKING;
+                            case 2:
+                                return Comandos.FACTURACION_ENTRE_FECHAS;
+                            case 3:
+                                return Comandos.FACTURACION_ABONADO;
+                            case 4:
+                                return Comandos.ABONO_ALTA;
+                            case 5:
+                                return Comandos.ABONO_MODIFICA;
+                            case 6:
+                                return Comandos.ABONO_BAJA;
+                            case 7:
+                                return Comandos.ABONO_CADUCIDAD;
+                            case 8:
+                                return Comandos.COPIA_SEGURIDAD_COPIAR;
+                            case 9:
+                                return Comandos.COPIA_SEGURIDAD_RESTAURAR;
+                        }
+                        break;
+                    case 3:
+                        return null;
+                }
+            } catch (InputMismatchException ime) {
                 System.out.println();
                 System.out.println("Ha introducido un caracter no valido, solo puede ingresar numeros");
             }
@@ -109,7 +109,7 @@ public class Menu {
 
         return null;
     }
-    
+
     public static void main(String[] args) {
         Menu.menu();
     }
