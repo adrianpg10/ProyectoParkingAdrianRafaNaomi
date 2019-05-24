f5016c4665cc1ac376e849f424a54f23256842af
Adrianpg <adrianperezgomezsmr@gmail.com>
2019-05-24 19:22:08 +0200

    Implementacion del metodo generarPin y creación del nuevo constructor por defecto.

--- src/main/java/daw/tickets/TicketsVO.java
+++ src/main/java/daw/tickets/TicketsVO.java
@@ -7,6 +7,7 @@ package daw.tickets;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Random;
 
 /**
  *
@@ -22,8 +23,11 @@ public class TicketsVO {
     private LocalDate fecfinpin;
     private LocalTime horaInicio;
     private LocalTime horaFin;
+    private static int numero=45;
+    private static int codigo=10000;
 
     //Método constructor parametrizado
+   
     public TicketsVO(int codticket, int numplaza, String matricula, String pin_desechable, LocalDate fecinipin, LocalDate fecfinpin, LocalTime horaInicio, LocalTime horaFin) {
         this.codticket = codticket;
         this.numplaza = numplaza;
@@ -34,9 +38,43 @@ public class TicketsVO {
         this.horaInicio = horaInicio;
         this.horaFin = horaFin;
     }
+
+    public TicketsVO(String matricula) {
+
+        this.matricula = matricula;
+        this.codticket = codigo;
+        this.numplaza = numero;
+        this.pin_desechable = generarPin();
+        this.fecinipin = LocalDate.now();
+        this.fecfinpin = LocalDate.now();
+        this.horaInicio = LocalTime.now();
+        this.horaFin = LocalTime.now();
+        
+        numero--;
+        codigo++;
+    }
+
+    // Metodo para generar el pin aleatorio
+    
+    private String generarPin() {
+        String pinDesechable = "";
+        Random aleatorio = new Random();
+        int generador;
+        for (int i = 0; i < 6; i++) {
+            generador = aleatorio.nextInt(10);
+            pinDesechable += generador;
+        }
+
+        return pinDesechable;
+    }
+
+
     
     //Constructor por defecto
     public TicketsVO() {
+        
+  
+        
     }
 
     //Métodos getters y setters
@@ -108,6 +146,14 @@ public class TicketsVO {
     @Override
     public String toString() {
         return "TicketsVO{" + "codticket=" + codticket + ", numplaza=" + numplaza + ", matricula=" + matricula + ", pin_desechable=" + pin_desechable + ", fecinipin=" + fecinipin + ", fecfinpin=" + fecfinpin + ", horaInicio=" + horaInicio + ", horaFin=" + horaFin + '}';
+    }
+    
+    public static void main(String[] args) {
+        
+        TicketsVO t = new TicketsVO();
+        
+        System.out.println(t.generarPin());
+        
     }
   
 }
