1c15e9c1803420ebee5fa4e9fe3b84617663022b
Rafa <rafaelcarrillobonilla@gmail.com>
2019-05-26 13:02:33 +0200

    Añade un constructor a vehiculos para poder pasarle una matricula

--- src/main/java/daw/vehiculos/VehiculoVO.java
+++ src/main/java/daw/vehiculos/VehiculoVO.java
@@ -18,6 +18,11 @@ public class VehiculoVO {
         this.tipoVehiculo = tipoVehiculo;
     }
 
+    public VehiculoVO(String matricula) {
+        this.matricula = matricula;
+    }
+
+    
     public VehiculoVO() {
     }
 
