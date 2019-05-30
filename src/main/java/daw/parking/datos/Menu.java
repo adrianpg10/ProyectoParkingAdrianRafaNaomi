ce95c581f4c81a46d741e7310ffc55fd9f8c908d
Adrianpg <adrianperezgomezsmr@gmail.com>
2019-05-30 18:45:45 +0200

    Modificamos el menú para que cuando realice una orden no finalice el programa

--- src/main/java/daw/parking/datos/Menu.java
+++ src/main/java/daw/parking/datos/Menu.java
@@ -15,92 +15,107 @@ import java.util.Scanner;
  */
 public class Menu {
 
-    private static Scanner teclado = new Scanner(System.in);
-    private static int opcion;
-
     // Creamos la estructura del menu teniendo en cuenta las excepciones que se puedan dar
     public static Comandos menu() {
 
-        do {
+        Scanner teclado = new Scanner(System.in);
 
-            System.out.println("==========================================");
-            System.out.println("===============PARKING DAW================");
-            System.out.println("==========================================");
-            System.out.println("1- Entrar en la zona clientes.");
-            System.out.println("2- Entrar en la zona administrador.");
+        System.out.println("==========================================");
+        System.out.println("===============PARKING DAW================");
+        System.out.println("==========================================");
+        System.out.println("1- Entrar en la zona clientes.");
+        System.out.println("2- Entrar en la zona administrador.");
+        System.out.println("3- Salir");
 
+        int opcion = teclado.nextInt();
+
+        while (!(opcion == 1 || opcion == 2 || opcion == 3)) {
+            System.out.println("La opción que has elegido es incorrecta ");
             opcion = teclado.nextInt();
+        }
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("Zona cliente. Elija una opción:");
-                    System.out.println("1- Depositar vehículo.");
-                    System.out.println("2- Retirar vehículo.");
-                    System.out.println("3- Depositar Abonado");
-                    System.out.println("4- Retirar Abonado");
-                    System.out.println("0- Volver al menú principal");
-                    opcion = teclado.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.println("Zona cliente. Elija una opción:");
+                System.out.println("1- Depositar vehículo.");
+                System.out.println("2- Retirar vehículo.");
+                System.out.println("3- Depositar Abonado");
+                System.out.println("4- Retirar Abonado");
+                System.out.println("0- Volver al menú principal");
+                int opcion2 = teclado.nextInt();
 
-                    switch (opcion) {
-                        case 0:
+                while (!(opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4 || opcion == 0)) {
+                    System.out.println("La opción que has elegido es incorrecta ");
+                    opcion2 = teclado.nextInt();
+                }
+                switch (opcion2) {
+                    case 0:
 
-                            return Comandos.VOLVER_MENU;
+                        return Comandos.VOLVER_MENU;
 
-                        case 1:
-                            return Comandos.DEPOSITAR_VEHICULO;
-                        case 2:
-                            return Comandos.RETIRAR_VEHICULO;
-                        case 3:
-                            return Comandos.DEPOSITAR_ABONADO;
-                        case 4:
-                            return Comandos.RETIRAR_ABONADO;
+                    case 1:
+                        return Comandos.DEPOSITAR_VEHICULO;
+                    case 2:
+                        return Comandos.RETIRAR_VEHICULO;
+                    case 3:
+                        return Comandos.DEPOSITAR_ABONADO;
+                    case 4:
+                        return Comandos.RETIRAR_ABONADO;
 
-                    }
-                    break;
+                }
+                break;
 
-                case 2:
-                    System.out.println("Zona administrador. Elija una opción:");
-                    System.out.println("1- Consultar estado del parking.");
-                    System.out.println("2- Facturación entre fechas.");
-                    System.out.println("3- Facturación abonado");
-                    System.out.println("4- Dar de alta un abonado.");
-                    System.out.println("5- Modificar datos abonado.");
-                    System.out.println("6- Dar de baja un abonado");
-                    System.out.println("7- Datos caducidad abonado.");
-                    System.out.println("8- Realizar copia de seguridad.");
-                    System.out.println("9- Restaurar copia de seguridad");
-                    System.out.println("0-Volver al menú");
-                    opcion = teclado.nextInt();
+            case 2:
+                System.out.println("Zona administrador. Elija una opción:");
+                System.out.println("1- Consultar estado del parking.");
+                System.out.println("2- Facturación entre fechas.");
+                System.out.println("3- Facturación abonado");
+                System.out.println("4- Dar de alta un abonado.");
+                System.out.println("5- Modificar datos abonado.");
+                System.out.println("6- Dar de baja un abonado");
+                System.out.println("7- Datos caducidad abonado.");
+                System.out.println("8- Realizar copia de seguridad.");
+                System.out.println("9- Restaurar copia de seguridad");
+                System.out.println("0-Volver al menú");
+                int opcion3 = teclado.nextInt();
+                while (opcion3 < 0 || opcion3 > 9) {
+                    System.out.println("La opción que has elegido es incorrecta ");
+                    opcion2 = teclado.nextInt();
+                }
 
-                    switch (opcion) {
-                        case 0:
-                            return Comandos.VOLVER_MENU;
-                        case 1:
-                            return Comandos.ESTADO_PARKING;
-                        case 2:
-                            return Comandos.FACTURACION_ENTRE_FECHAS;
-                        case 3:
-                            return Comandos.FACTURACION_ABONADO;
-                        case 4:
-                            return Comandos.ABONO_ALTA;
-                        case 5:
-                            return Comandos.ABONO_MODIFICA;
-                        case 6:
-                            return Comandos.ABONO_BAJA;
-                        case 7:
-                            return Comandos.ABONO_CADUCIDAD;
-                        case 8:
-                            return Comandos.COPIA_SEGURIDAD_COPIAR;
-                        case 9:
-                            return Comandos.COPIA_SEGURIDAD_RESTAURAR;
-                    }
-                    break;
+                switch (opcion3) {
+                    case 0:
+                        return Comandos.VOLVER_MENU;
+                    case 1:
+                        return Comandos.ESTADO_PARKING;
+                    case 2:
+                        return Comandos.FACTURACION_ENTRE_FECHAS;
+                    case 3:
+                        return Comandos.FACTURACION_ABONADO;
+                    case 4:
+                        return Comandos.ABONO_ALTA;
+                    case 5:
+                        return Comandos.ABONO_MODIFICA;
+                    case 6:
+                        return Comandos.ABONO_BAJA;
+                    case 7:
+                        return Comandos.ABONO_CADUCIDAD;
+                    case 8:
+                        return Comandos.COPIA_SEGURIDAD_COPIAR;
+                    case 9:
+                        return Comandos.COPIA_SEGURIDAD_RESTAURAR;
 
-            }
+                    default:
+                        throw new AssertionError();
+                }
+                
+            case 3:
+                return Comandos.SALIR;
+                
+            default:
+                throw new AssertionError();
 
-            System.out.println();
-
-        } while (opcion != 0);
+        }
         return null;
 
     }
